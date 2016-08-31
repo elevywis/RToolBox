495d16a0388d897dfa24818d3b0a1fb572d56a0b
elevywis <emmanuel.levy@weizmann.ac.il>
2016-08-31 17:25:52 +0300

    new human proteome file

---
+++ humanProteome.R
@@ -0,0 +1,29 @@
+
+
+
+
+###
+### This function will create a matrix where each line is a UNIPROT id and each column an information
+### We can start with the following columns:
+### - UNIPROT ID
+### - Gene name
+### - OTHER database's IDs (Ensembl, seqref)
+### - description
+###
+get.REF.hs = function(){
+
+    ## Use BioMart??
+    ## Human annotation package??
+
+    ## Check the interaction data in Biogrid --> what are the main IDs that they use?
+}
+
+###
+### This function takes a "reference proteome table" and returns matching proteomic information
+###
+get.proteome.table.hs = function(){
+
+}
+
+
+### 
